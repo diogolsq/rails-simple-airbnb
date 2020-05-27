7899799bf3fcc6898089b6b37d2844241a0120ba
Diogo Queiroz <diogolsq@poli.ufrj.br>
2020-05-27 14:54:44 -0300

    setting up the bootstrap

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,47 @@
 class FlatsController < ApplicationController
+
+
+  before_action :find_flat, only: [:show, :edit, :update, :destroy]
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flats_params)
+    @flat.save
+
+    redirect_to flats_path
+  end
+
+  def edit
+  end
+
+  def update
+    flat.update(flats_params)
+
+    redirect_to task_path
+  end
+
+  def destroy
+    @flat.destroy
+
+    redirect_to flats_path
+  end
+
+  private
+
+  def find_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def flats_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+
+Flat.create!(
+  name: 'Eagles Nest',
+  address: 'Firenze av. Paul de Marco number 33',
+  description: 'Where the assassins meet',
+  price_per_night: 50,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Recanto do Guerreiro',
+  address: 'Rua dos Prazeres 38',
+  description: " you really don't wanna see a description",
+  price_per_night: 35,
+  number_of_guests: 5
+)
+
+Flat.create!(
+  name: 'Pineapple House',
+  address: 'Bikini Bottom',
+  description: 'lovely place in the middle of the pacific',
+  price_per_night: 50,
+  number_of_guests: 2
+)
